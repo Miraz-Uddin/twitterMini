513e79173709fe250c167526ac361aab9122e8f7
Md. Miraz Uddin <mirazuddin0623@gmail.com>
2022-03-14 21:17:23 +0600

    Read Data from Storage

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,17 +1,14 @@
 // IIFE
 (function(){
 
-    // Start your code here
-    console.log('JS Connected');
-
     const TWEET_INPUT = document.querySelector('#tweet_input');
     const TWEET_SUBMIT_BTN = document.querySelector('#tweet_submit_btn');
+    let tweetList = readDataFromStorage();
 
     TWEET_INPUT.addEventListener('input',function(e){
         e.preventDefault();
         const characterCount= document.querySelector('#characters');
         const characterMax= document.querySelector('#characters_max');
-        const tweetubmit_btn= document.querySelector('#tweet_submit_btn');
         characterCount.textContent = this.value.length; 
 
         if(tweetMaxCount(this.value.length,characterMax.textContent)){
@@ -22,13 +19,21 @@
             TWEET_SUBMIT_BTN.classList.remove('d-none');
             TWEET_SUBMIT_BTN.classList.add('d-none');
         }
-
     });
 
     function tweetMaxCount(inputLength, count){
         if(inputLength>parseInt(count))
             return false;
         return true;
+    }
+
+    function readDataFromStorage(){
+        if(localStorage.getItem('tweetList')){
+            let arr = JSON.parse(localStorage.getItem('tweetList'));
+            return arr;
+        }else{
+            return [];
+        }
     }
 
 
